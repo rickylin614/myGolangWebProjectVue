a05aae6f866c96dbe104fabc9605babd61d5b3da
sheldonsiao <sheldon.siao@timecitytech.com>
2021-01-29 16:34:54 +0800

    商店 CRUD 修改
    # Conflicts:
    #	src/router/index.js
    
    商店 CRUD 新增檔案沒上到

--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,10 @@ import login from '@/views/login'
 import register from '@/views/register'
 import home from '@/components/Nav'
 import userManage from '@/views/user/userManage'
-import loginRecord from '@/views/user/loginRecord'
+import loginRecord from '@/views/user/loginRecord'import orderManage from '@/views/order/orderManage'
+import updateOrder from '@/views/order/updateOrder'
+import insertOrder from '@/views/order/insertOrder'
 // import test2 from '@/views/user/test2'
-
 Vue.use(Router)
 
 const routerMap = [
@@ -30,6 +31,28 @@ const routerMap = [
         path:'/loginRecord',
         name:'登入記錄',
         component:loginRecord
+      }
+      ,
+      {
+        path:'/orderManage',
+        name:'店家訂單',
+        component:orderManage
+      }
+      ,
+      {
+        path: '/insertOrder',
+        name: '新增餐廳',
+        component:  insertOrder
+      },
+      {
+        path: '/updateOrder',
+        name: '修改餐廳',
+        component:  updateOrder
+      },
+      {
+        path:'/userManager2',
+        name:'頁面二',
+        component:test2
       }
     ]
   },
